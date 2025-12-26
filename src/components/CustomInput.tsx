e3aba5f1853b6481e5b02e2b124a534fc2dec05b
Sourabh Sharma <sourabh20905@gmail.com>
2025-12-26 23:26:27 +0530

    prop is added

--- src/components/CustomInput.tsx
+++ src/components/CustomInput.tsx
@@ -1,7 +1,16 @@
-const CustomInput = () => {
+interface CustomInputProps {
+  value: string;
+  onChange: (value: string) => void;
+}
+
+const CustomInput = ({ value, onChange }: CustomInputProps) => {
   return (
     <div>
-      <input type="text" />
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+      />
     </div>
   );
 };
