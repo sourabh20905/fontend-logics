2cf67448fe132db37a392e078124f93575956ea2
Sourabh Sharma <sourabh20905@gmail.com>
2025-12-29 07:58:24 +0530

    feat: handleStoryUpload added for instagram story

--- src/container/InstaGramStory/InstaGramStory.tsx
+++ src/container/InstaGramStory/InstaGramStory.tsx
@@ -1,11 +1,40 @@
+import Image from "next/image";
+import { useState } from "react";
+
+interface Story {
+  id: string;
+  url: string;
+}
+
 const InstaGramStory = () => {
-  
+  const [stories, setStories] = useState<Story[]>([]);
+  const handleSToryUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    const id = crypto.randomUUID();
+    const url = URL.createObjectURL(file || new Blob());
+    console.log(url, "url");
+    setStories((prev: Story[]) => [{ id, url }, ...prev]);
+    console.log(stories);
+  };
   return (
     <div>
       <div>
-        <label htmlFor="fileInput">+</label>
-        <input type="file" hidden id="fileInput" />
+        <label
+          htmlFor="fileInput"
+          className="border border-gray-300 rounded-full p-2"
+        >
+          +
+        </label>
+        <input
+          type="file"
+          hidden
+          id="fileInput"
+          onChange={(e) => handleSToryUpload(e)}
+        />
       </div>
+      {stories.map((item) => {
+        return <Image key={item.id} src={item.url} alt="story" width={100} height={100} />;
+      })}
     </div>
   );
 };
