8581c6ca1a5cf8e9fc385053ae4e33fcdfb5907a
Sourabh Sharma <sourabh20905@gmail.com>
2025-12-29 07:36:34 +0530

    feat: instagram story feature

---
+++ app/instagramstory/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+import InstaGramStory from "@/src/container/InstaGramStory/InstaGramStory";
+
+const InstaGramStoryPage = () => {
+  return <InstaGramStory />;
+};
+
+export default InstaGramStoryPage;
---
+++ src/container/InstaGramStory/InstaGramStory.tsx
@@ -0,0 +1,13 @@
+const InstaGramStory = () => {
+  
+  return (
+    <div>
+      <div>
+        <label htmlFor="fileInput">+</label>
+        <input type="file" hidden id="fileInput" />
+      </div>
+    </div>
+  );
+};
+
+export default InstaGramStory;
