5405e08f468babde9c5626158cc2507d07e2f511
Sourabh Sharma <sourabh20905@gmail.com>
2025-12-29 08:29:22 +0530

    feat: previous story selection

--- src/container/InstaGramStory/InstaGramStory.tsx
+++ src/container/InstaGramStory/InstaGramStory.tsx
@@ -9,6 +9,7 @@ interface Story {
 const InstaGramStory = () => {
   const [stories, setStories] = useState<Story[]>([]);
   const timeoutRefs = useRef<NodeJS.Timeout[]>([]);
+  const fileInputRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
     return () => {
@@ -16,29 +17,48 @@ const InstaGramStory = () => {
         clearTimeout(timeoutId);
       });
       timeoutRefs.current = [];
+      // Clean up all object URLs on unmount
+      stories.forEach((story) => {
+        URL.revokeObjectURL(story.url);
+      });
     };
-  }, []);
+  }, [stories]);
 
   const handleSToryUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
-    const id = crypto.randomUUID();
-    const url = URL.createObjectURL(file || new Blob()); // make sure this should be remove as well because this is webapi so this will take memorie of browser
-    console.log(url, "url");
-    setStories((prev: Story[]) => [{ id, url }, ...prev]);
-    const filteredStories = stories.filter((story) => {
-      if (story.id === id) {
-        URL.revokeObjectURL(story.url);
-      } else {
-        return true;
-      }
-    });
-    const timeoutId = setTimeout(() => {
-      setStories(filteredStories);
-    }, 2000);
+    if (!file) return;
 
-    console.log(timeoutId, "timeoutId");
-    timeoutRefs.current.push(timeoutId);
-    console.log(stories);
+    console.log(file, "file");
+    const id = crypto.randomUUID();
+    const url = URL.createObjectURL(file); // make sure this should be remove as well because this is webapi so this will take memorie of browser
+    console.log(url, "url");
+
+    // Add new story to the beginning
+    setStories((prev: Story[]) => {
+      const newStories = [{ id, url }, ...prev];
+
+      // Set up timeout to remove this story after 2 seconds
+      const timeoutId = setTimeout(() => {
+        setStories((currentStories) => {
+          const filteredStories = currentStories.filter((story) => {
+            if (story.id === id) {
+              URL.revokeObjectURL(story.url);
+              return false;
+            }
+            return true;
+          });
+          return filteredStories;
+        });
+      }, 2000);
+
+      timeoutRefs.current.push(timeoutId);
+      return newStories;
+    });
+
+    // Reset file input so the same file can be selected again
+    if (fileInputRef.current) {
+      fileInputRef.current.value = "";
+    }
   };
 
   return (
@@ -51,10 +71,11 @@ const InstaGramStory = () => {
           +
         </label>
         <input
+          ref={fileInputRef}
           type="file"
           hidden
           id="fileInput"
-          onChange={(e) => handleSToryUpload(e)}
+          onChange={(e) => handleSToryUpload(e)} // onchange is not work if same value of file
         />
       </div>
       {stories.map((item) => {
