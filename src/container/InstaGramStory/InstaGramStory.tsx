9ab5c3004360ac999ae2a984e1605780bb743a1d
Sourabh Sharma <sourabh20905@gmail.com>
2025-12-29 08:22:19 +0530

    feat: cleanup of timers

--- src/container/InstaGramStory/InstaGramStory.tsx
+++ src/container/InstaGramStory/InstaGramStory.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 interface Story {
   id: string;
@@ -8,14 +8,39 @@ interface Story {
 
 const InstaGramStory = () => {
   const [stories, setStories] = useState<Story[]>([]);
+  const timeoutRefs = useRef<NodeJS.Timeout[]>([]);
+
+  useEffect(() => {
+    return () => {
+      timeoutRefs.current.forEach((timeoutId) => {
+        clearTimeout(timeoutId);
+      });
+      timeoutRefs.current = [];
+    };
+  }, []);
+
   const handleSToryUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
     const id = crypto.randomUUID();
-    const url = URL.createObjectURL(file || new Blob());
+    const url = URL.createObjectURL(file || new Blob()); // make sure this should be remove as well because this is webapi so this will take memorie of browser
     console.log(url, "url");
     setStories((prev: Story[]) => [{ id, url }, ...prev]);
+    const filteredStories = stories.filter((story) => {
+      if (story.id === id) {
+        URL.revokeObjectURL(story.url);
+      } else {
+        return true;
+      }
+    });
+    const timeoutId = setTimeout(() => {
+      setStories(filteredStories);
+    }, 2000);
+
+    console.log(timeoutId, "timeoutId");
+    timeoutRefs.current.push(timeoutId);
     console.log(stories);
   };
+
   return (
     <div>
       <div>
@@ -33,7 +58,15 @@ const InstaGramStory = () => {
         />
       </div>
       {stories.map((item) => {
-        return <Image key={item.id} src={item.url} alt="story" width={100} height={100} />;
+        return (
+          <Image
+            key={item.id}
+            src={item.url}
+            alt="story"
+            width={100}
+            height={100}
+          />
+        );
       })}
     </div>
   );
