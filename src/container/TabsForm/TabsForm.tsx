29472ffdf41695f9620d660727b7ab0ee563e627
Sourabh Sharma <sourabh20905@gmail.com>
2026-01-05 06:49:17 +0530

    feat: implement tab navigation in TabsForm component

--- src/container/TabsForm/TabsForm.tsx
+++ src/container/TabsForm/TabsForm.tsx
@@ -1,5 +1,41 @@
+import { useState } from "react";
+import Interest from "./Interests";
+import Profile from "./Profile";
+import Settings from "./Settings";
+
 const TabsForm = () => {
-  return <div></div>;
+  const [activeTab, setActiveTab] = useState(0);
+  const tabs = [
+    {
+      name: "Profile",
+      component: <Profile />,
+    },
+    {
+      name: "Settings",
+      component: <Settings />,
+    },
+    {
+      name: "Interest",
+      component: <Interest />,
+    },
+  ];
+
+  const activeTabComponent = tabs[activeTab];
+
+  return (
+    <div className="w-full h-full flex items-center justify-center">
+      <div className="">
+        {tabs.map((tab, index) => {
+          return (
+            <div key={tab.name} onClick={() => setActiveTab(index)}>
+              {tab.name}
+            </div>
+          );
+        })}
+        <div>{activeTabComponent.component}</div>
+      </div>
+    </div>
+  );
 };
 
 export default TabsForm;
