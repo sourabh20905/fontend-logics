be4a32608b124d5f2414670be0d06d5f9551715a
Sourabh Sharma <sourabh20905@gmail.com>
2026-01-06 08:52:22 +0530

    feat: ui added for the nested check box

--- src/container/NestedCheckBox(imp)/NestedCheckBox.tsx
+++ src/container/NestedCheckBox(imp)/NestedCheckBox.tsx
@@ -1,5 +1,69 @@
+import { useState } from "react";
+import { checkboxes } from "./config";
+
+interface CheckboxNode {
+  id: number;
+  name: string;
+  children?: CheckboxNode[];
+}
+
+interface CheckboxesProps {
+  data: CheckboxNode[];
+  checked: { [key: number]: boolean };
+  setChecked: React.Dispatch<React.SetStateAction<{ [key: number]: boolean }>>;
+}
+
 const NestedCheckBox = () => {
-  return <div>NestedCheckBox</div>;
+  const [checked, setChecked] = useState<{ [key: number]: boolean }>({
+    1: false,
+  });
+
+  const Checkboxes = ({ data, checked, setChecked }: CheckboxesProps) => {
+    if (!data || !Array.isArray(data)) {
+      return null;
+    }
+
+    const handleChange = (
+      e: React.ChangeEvent<HTMLInputElement>,
+      node: CheckboxNode
+    ) => {
+      setChecked((prev) => {
+        console.log(prev);
+        return {
+          ...prev,
+          [node.id]: e.target.checked,
+        };
+      });
+    };
+
+    return (
+      <div>
+        {data.map((node: CheckboxNode) => (
+          <div key={node.id} className="ml-6">
+            <input
+              type="checkbox"
+              name={node.name}
+              checked={checked[node.id] || false}
+              onChange={(e) => handleChange(e, node)}
+            />
+            {node.name}
+            {node.children && (
+              <Checkboxes
+                data={node.children}
+                checked={checked}
+                setChecked={setChecked}
+              />
+            )}
+          </div>
+        ))}
+      </div>
+    );
+  };
+  return (
+    <div>
+      <Checkboxes data={checkboxes} checked={checked} setChecked={setChecked} />
+    </div>
+  );
 };
 
 export default NestedCheckBox;
---
+++ src/container/NestedCheckBox(imp)/config.ts
@@ -0,0 +1,40 @@
+export const checkboxes = [
+  {
+    id: 1,
+    name: "fruit",
+    children: [
+      {
+        id: 2,
+        name: "citrus",
+        children: [
+          {
+            id: 3,
+            name: "orange",
+          },
+          {
+            id: 4,
+            name: "lemon",
+          },
+        ],
+      },
+      {
+        id: 5,
+        name: "berries",
+        children: [
+          {
+            id: 7,
+            name: "strawberry",
+          },
+          {
+            id: 8,
+            name: "blueberry",
+          },
+        ],
+      },
+      {
+        id: 6,
+        name: "apple",
+      },
+    ],
+  },
+];
