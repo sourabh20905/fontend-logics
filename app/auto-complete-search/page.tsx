65a376bda8b27a5ba3607326f7e40191bc8ca951
Sourabh Sharma <sourabh20905@gmail.com>
2026-01-10 08:46:56 +0530

    feat: added debounce

--- app/auto-complete-search/page.tsx
+++ app/auto-complete-search/page.tsx
@@ -1,6 +1,16 @@
 import AutoCompleteSearchBar from "@/src/container/AutoCompleteSearchBar/AutoCompleteSearchBar";
 
-const AutoCompleteSearchBarPage = () => {
+const AutoCompleteSearchBarPage = async () => {
+//   try {
+//     const response = await fetch(
+//       "https://dummyjson.com/products/search?q=phone"
+//     );
+//     const data = await response.json();
+//     console.log(data);
+//   } catch (err) {
+//     console.log(err);
+//   }
+
   return (
     <div>
       <AutoCompleteSearchBar />
--- src/container/AutoCompleteSearchBar/AutoCompleteSearchBar.tsx
+++ src/container/AutoCompleteSearchBar/AutoCompleteSearchBar.tsx
@@ -1,7 +1,37 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import style from "./AutoCompleteSearchBar.module.css";
 
 const AutoCompleteSearchBar = () => {
-  return <div className={style.root}>hello</div>;
+  const [input, setInput] = useState("");
+  const fetchProducts = async (input: string) => {
+    try {
+      const response = await fetch(
+        `https://dummyjson.com/products/search?q=${input}`
+      );
+      const data = await response.json();
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => fetchProducts(input), 300);
+    return () => clearTimeout(timer);
+  }, [input]);
+
+  return (
+    <div className={style.root}>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        className={style.input}
+      />
+    </div>
+  );
 };
 
 export default AutoCompleteSearchBar;
